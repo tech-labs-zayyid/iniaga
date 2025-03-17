945dc269f50b0526a715b0dd20242deacf170e93
arif.romdon <arif.romdon@nabatisnack.co.id>
2025-03-18 00:17:19 +0700

    STYLE(Navbar): Revamp navbar for mobile view

--- components/global/moleculs/Navbar.tsx
+++ components/global/moleculs/Navbar.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles, { component } from "@styles/style";
-import { close, logoDark, logo1, menu } from "@/public/assets";
+import { close, logo1, menu, icArrowRight } from "@/public/assets";
 import { navLinks } from "@/constants";
 
 const Navbar: React.FC = () => {
@@ -13,11 +13,12 @@ const Navbar: React.FC = () => {
     <nav className="w-full flex py-6 justify-between items-center navbar relative">
       <Image src={logo1} priority alt="Iniaga" width={134} height={40} />
 
+      {/* Menu Desktop */}
       <ul className="list-none sm:flex hidden justify-center items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`  font-normal cursor-pointer text-white ${
+            className={`font-normal cursor-pointer text-white ${
               index === navLinks.length - 1 ? "mr-0" : "mr-10"
             }`}
           >
@@ -35,42 +36,62 @@ const Navbar: React.FC = () => {
         </Link>
       </div>
 
+      {/* Mobile Menu */}
       <div className="sm:hidden flex flex-1 justify-end items-center">
-        <button className="rounded-xl p-4 bg-[#247bfe]" onClick={() => setToggle((prev) => !prev)}>
+        <button
+          className="rounded-xl p-4 bg-[#247bfe] absolute z-10"
+          onClick={() => setToggle((prev) => !prev)}
+        >
           <Image
             src={toggle ? close : menu}
             alt="menu"
-            className="object-contain"
+            className="object-contain w-[24px] h-[24px]"
             width={28}
             height={28}
           />
         </button>
+
+        {/* Menu Overlay Fullscreen */}
         <div
-          className={`${
-            toggle ? "flex" : "hidden"
-          } p-6 bg-black-gradient absolute top-20 right-0 ring-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          className={`fixed top-0 left-0 w-full h-screen bg-black/100 flex flex-col items-center justify-center transition-opacity duration-300 ${
+            toggle ? "opacity-100 visible" : "opacity-0 invisible"
+          }`}
         >
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`  font-normal cursor-pointer text-[16px] text-white ${
-                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
-                }`}
-              >
-                <Link href={`#${nav.id}`}>{nav.title}</Link>
+          <ul className="list-none w-full flex flex-col items-start space-y-10 my-auto mr-auto px-6">
+            {navLinks.map((nav) => (
+              <li key={nav.id} className="text-white text-2xl font-medium">
+                <Link href={`#${nav.id}`} onClick={() => setToggle(false)}>
+                  {nav.title}
+                </Link>
               </li>
             ))}
-            <li className="mt-4">
+            <li className="w-full">
               <Link
                 href="#pricingPlant"
-                className="bg-black text-white px-4 py-2 rounded-full font-semibold text-sm shadow-md hover:bg-white hover:text-black transition"
+                className={`px-6 py-3 sm:px-8 sm:py-4 !w-[100%] ${component.btnPrimary} flex items-center justify-center gap-2 group`}
+                onClick={() => setToggle(false)}
               >
-                Register
+                <span className="leading-[32px]">Registration</span>
+                <Image
+                  src={icArrowRight}
+                  alt="arrow"
+                  className="w-[20px] h-auto transition-transform duration-500 group-hover:-translate-y-1 group-hover:translate-x-1"
+                />
               </Link>
             </li>
           </ul>
+
+          {/* Gambar Pindah ke Paling Bawah */}
+          <Image
+            src={logo1}
+            priority
+            alt="Iniaga"
+            width={134}
+            height={40}
+            className="mt-auto pb-10"
+          />
         </div>
+
       </div>
     </nav>
   );
