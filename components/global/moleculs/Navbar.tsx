4ad0ff6ae045ef3db9d61977316b1806d0479994
arif.romdon <arif.romdon@nabatisnack.co.id>
2025-03-26 02:17:08 +0700

    REFACTOR: navbar component

--- components/global/moleculs/Navbar.tsx
+++ components/global/moleculs/Navbar.tsx
@@ -2,8 +2,14 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import styles, { component } from "@styles/style";
-import { close, logo1, menu, icArrowRight } from "@/public/assets";
+import { component } from "@styles/style";
+import {
+  icMenuClose,
+  logo1,
+  icMenuOpen,
+  icArrowRight,
+  icChevronRight,
+} from "@/public/assets";
 import { navLinks } from "@/constants";
 
 const Navbar: React.FC = () => {
@@ -18,7 +24,7 @@ const Navbar: React.FC = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-normal cursor-pointer text-white ${
+            className={`font-normal cursor-pointer text-white transition-all duration-300 hover:text-blue-400 ${
               index === navLinks.length - 1 ? "mr-0" : "mr-10"
             }`}
           >
@@ -28,10 +34,7 @@ const Navbar: React.FC = () => {
       </ul>
 
       <div className="hidden sm:flex">
-        <Link
-          href="#pricingPlant"
-          className={`ss:w-full ${component.btnPrimary}`}
-        >
+        <Link href="#pricingPlant" className={`ss:w-full ${component.btnPrimary}`}>
           Register
         </Link>
       </div>
@@ -39,36 +42,47 @@ const Navbar: React.FC = () => {
       {/* Mobile Menu */}
       <div className="sm:hidden flex flex-1 justify-end items-center">
         <button
-          className="rounded-xl p-4 bg-[#247bfe] absolute z-10"
+          className="rounded-xl p-2 bg-[#247bfe] absolute z-10 transition-transform duration-300"
           onClick={() => setToggle((prev) => !prev)}
         >
           <Image
-            src={toggle ? close : menu}
+            src={toggle ? icMenuClose : icMenuOpen}
             alt="menu"
-            className="object-contain w-[24px] h-[24px]"
-            width={28}
-            height={28}
+            className="object-contain w-[34px] h-[34px]"
+            width={34}
+            height={34}
           />
         </button>
 
         {/* Menu Overlay Fullscreen */}
         <div
-          className={`fixed top-0 left-0 w-full h-screen bg-black/100 flex flex-col items-center justify-center transition-opacity duration-300 ${
+          className={`fixed top-0 left-0 w-full h-screen bg-black/90 flex flex-col items-center justify-center transition-opacity duration-300 ${
             toggle ? "opacity-100 visible" : "opacity-0 invisible"
           }`}
         >
-          <ul className="list-none w-full flex flex-col items-start space-y-10 my-auto mr-auto px-6">
+          <ul className="list-none w-full flex flex-col items-start space-y-6 my-auto px-6">
             {navLinks.map((nav) => (
-              <li key={nav.id} className="text-white text-2xl font-medium">
-                <Link href={`#${nav.id}`} onClick={() => setToggle(false)}>
+              <li key={nav.id} className="w-full">
+                <Link
+                  href={`#${nav.id}`}
+                  className="flex items-center justify-between text-white text-xl font-medium w-full p-4 rounded-lg transition-all duration-300 hover:bg-white/10 group"
+                  onClick={() => setToggle(false)}
+                >
                   {nav.title}
+                  <Image
+                    src={icChevronRight}
+                    alt="chevron"
+                    className="w-[24px] h-[24px] transition-transform duration-300 group-hover:translate-x-2 group-active:rotate-90"
+                    width={24}
+                    height={24}
+                  />
                 </Link>
               </li>
             ))}
             <li className="w-full">
               <Link
                 href="#pricingPlant"
-                className={`px-6 py-3 sm:px-8 sm:py-4 !w-[100%] ${component.btnPrimary} flex items-center justify-center gap-2 group`}
+                className={`px-6 py-3 sm:px-8 sm:py-4 w-full ${component.btnPrimary} flex items-center justify-center gap-2 group`}
                 onClick={() => setToggle(false)}
               >
                 <span className="leading-[32px]">Registration</span>
@@ -81,7 +95,7 @@ const Navbar: React.FC = () => {
             </li>
           </ul>
 
-          {/* Gambar Pindah ke Paling Bawah */}
+          {/* Logo di Bawah */}
           <Image
             src={logo1}
             priority
@@ -91,7 +105,6 @@ const Navbar: React.FC = () => {
             className="mt-auto pb-10"
           />
         </div>
-
       </div>
     </nav>
   );
